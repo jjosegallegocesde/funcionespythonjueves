05c1cb39f3bcaede816c839370bc8345d13a6fb8
308 <308@academicos.cesde.edu.co>
2022-09-22 07:42:22 -0500

    NTJ SE CREA LA FUNCION DE TEMPERATURA

---
+++ deibysanchez/funcion2.py
@@ -0,0 +1,16 @@
+#DECLARANDO FUNCIONES
+
+from mimetypes import init
+from turtle import width
+
+
+def calcularTemperatura(num):
+    numeros=[]
+    contador=0
+
+    while(numeros!=0):
+        temp= int(print("Digite la temeperatura: "))
+        numeros.append(temp)
+        contador += 1
+
+        if(contador==2)
